3f5c05c310e48781350d20f652a1700b62680e68
Nicolás Tobo <na.tobo@uniandes.edu.co>
2020-10-04 14:00:16 -0500

    animation complete

--- src/ThreePointVis/instancedPoints.js
+++ src/ThreePointVis/instancedPoints.js
@@ -1,30 +1,40 @@
 import * as React from 'react';
 import * as THREE from 'three';
-import { useLayout } from './layouts';
+import { useAnimatedLayout } from './layouts';
 
 // re-use for instance computations
 const scratchObject3D = new THREE.Object3D();
 
+function updateInstancedMeshMatrices({ mesh, data }) {
+  if (!mesh) return;
+
+  // set the transform matrix for each instance
+  for (let i = 0; i < data.length; ++i) {
+    const { x, y, z } = data[i];
+
+    scratchObject3D.position.set(x, y, z);
+    scratchObject3D.rotation.set(0.5 * Math.PI, 0, 0); // cylinders face z direction
+    scratchObject3D.updateMatrix();
+    mesh.setMatrixAt(i, scratchObject3D.matrix);
+  }
+
+  mesh.instanceMatrix.needsUpdate = true;
+}
+
 const InstancedPoints = ({ data, layout }) => {
-  useLayout({ data, layout });
   const meshRef = React.useRef();
   const numPoints = data.length;
-
+   // run the layout, animating on change
+   useAnimatedLayout({
+    data,
+    layout,
+    onFrame: () => {
+      updateInstancedMeshMatrices({ mesh: meshRef.current, data });
+    },
+   });
   // update instance matrices only when needed
   React.useEffect(() => {
-    const mesh = meshRef.current;
-
-    // set the transform matrix for each instance
-    for (let i = 0; i < data.length; ++i) {
-      const { x, y, z } = data[i];
-
-      scratchObject3D.position.set(x, y, z);
-      scratchObject3D.rotation.set(0.5 * Math.PI, 0, 0); // cylinders face z direction
-      scratchObject3D.updateMatrix();
-      mesh.setMatrixAt(i, scratchObject3D.matrix);
-    }
-
-    mesh.instanceMatrix.needsUpdate = true;
+    updateInstancedMeshMatrices({ mesh: meshRef.current, data });
   }, [data, layout]);
 
   return (
--- src/ThreePointVis/layouts.js
+++ src/ThreePointVis/layouts.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useSpring } from 'react-spring/three';
 
 function gridLayout(data) {
   const numPoints = data.length;
@@ -43,3 +44,55 @@ export const useLayout = ({ data, layout = 'grid' }) => {
     }
   }, [data, layout]);
 };
+
+function useSourceTargetLayout({ data, layout }) {
+  // prep for new animation by storing source
+  React.useEffect(() => {
+    for (let i = 0; i < data.length; ++i) {
+      data[i].sourceX = data[i].x || 0;
+      data[i].sourceY = data[i].y || 0;
+      data[i].sourceZ = data[i].z || 0;
+    }
+  }, [data, layout]);
+
+  // run layout
+  useLayout({ data, layout });
+
+  // store target
+  React.useEffect(() => {
+    for (let i = 0; i < data.length; ++i) {
+      data[i].targetX = data[i].x;
+      data[i].targetY = data[i].y;
+      data[i].targetZ = data[i].z;
+    }
+  }, [data, layout]);
+}
+
+function interpolateSourceTarget(data, progress) {
+  for (let i = 0; i < data.length; ++i) {
+    data[i].x = (1 - progress) * data[i].sourceX + progress * data[i].targetX;
+    data[i].y = (1 - progress) * data[i].sourceY + progress * data[i].targetY;
+    data[i].z = (1 - progress) * data[i].sourceZ + progress * data[i].targetZ;
+  }
+}
+
+export function useAnimatedLayout({ data, layout, onFrame }) {
+  // compute layout remembering initial position as source and
+  // end position as target
+  useSourceTargetLayout({ data, layout });
+
+  // do the actual animation when layout changes
+  const prevLayout = React.useRef(layout);
+  useSpring({
+    animationProgress: 1,
+    from: { animationProgress: 0 },
+    reset: layout !== prevLayout.current,
+    onFrame: ({ animationProgress }) => {
+      // interpolate based on progress
+      interpolateSourceTarget(data, animationProgress);
+      // callback to indicate data has updated
+      onFrame({ animationProgress });
+    },
+  });
+  prevLayout.current = layout;
+}
