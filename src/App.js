9185f3566be0a83ebd9d031f1bbbedcd95cc5ce7
Sofia Alvarez <ms.alvarezl@uniandes.edu.co>
2020-10-04 16:12:48 -0500

    Corregido

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import * as React from 'react';
 import ThreePointVis from './ThreePointVis/ThreePointVis';
 import './styles.css';
 
-const data = new Array(1056).fill(0).map((d, id) => ({ id }));
+const data = new Array(10000).fill(0).map((d, id) => ({ id }));
 
 export default function App() {
   const [layout, setLayout] = React.useState('grid');
+  const [selectedPoint, setSelectedPoint] = React.useState(null);
 
   return (
     <div className="App">
       <div className="vis-container">
-        <ThreePointVis data={data} layout={layout} />
+        <ThreePointVis
+          data={data}
+          layout={layout}
+          selectedPoint={selectedPoint}
+          onSelectPoint={setSelectedPoint}
+        />
       </div>
       <div className="controls">
         <strong>Layouts</strong>{' '}
@@ -26,6 +32,11 @@ export default function App() {
         >
           Spiral
         </button>
+        {selectedPoint && (
+          <div className="selected-point">
+            You selected <strong>{selectedPoint.id}</strong>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/ThreePointVis/instancedPoints.js
+++ src/ThreePointVis/instancedPoints.js
@@ -21,30 +21,113 @@ function updateInstancedMeshMatrices({ mesh, data }) {
   mesh.instanceMatrix.needsUpdate = true;
 }
 
-const InstancedPoints = ({ data, layout }) => {
+const SELECTED_COLOR = '#6f6';
+const DEFAULT_COLOR = '#fff';
+
+// re-use for instance computations
+const scratchColor = new THREE.Color();
+
+const usePointColors = ({ data, selectedPoint }) => {
+  const numPoints = data.length;
+  const colorAttrib = React.useRef();
+  const colorArray = React.useMemo(() => new Float32Array(numPoints * 3), [
+    numPoints,
+  ]);
+
+  React.useEffect(() => {
+    for (let i = 0; i < data.length; ++i) {
+      scratchColor.set(
+        data[i] === selectedPoint ? SELECTED_COLOR : DEFAULT_COLOR
+      );
+      scratchColor.toArray(colorArray, i * 3);
+    }
+    colorAttrib.current.needsUpdate = true;
+  }, [data, selectedPoint, colorArray]);
+
+  return { colorAttrib, colorArray };
+};
+
+const useMousePointInteraction = ({ data, selectedPoint, onSelectPoint }) => {
+  // track mousedown position to skip click handlers on drags
+  const mouseDownRef = React.useRef([0, 0]);
+  const handlePointerDown = e => {
+    mouseDownRef.current[0] = e.clientX;
+    mouseDownRef.current[1] = e.clientY;
+  };
+
+  const handleClick = event => {
+    const { instanceId, clientX, clientY } = event;
+    const downDistance = Math.sqrt(
+      Math.pow(mouseDownRef.current[0] - clientX, 2) +
+        Math.pow(mouseDownRef.current[1] - clientY, 2)
+    );
+
+    // skip click if we dragged more than 5px distance
+    if (downDistance > 5) {
+      event.stopPropagation();
+      return;
+    }
+
+    // index is instanceId if we never change sort order
+    const index = instanceId;
+    const point = data[index];
+
+    console.log('got point =', point);
+    // toggle the point
+    if (point === selectedPoint) {
+      onSelectPoint(null);
+    } else {
+      onSelectPoint(point);
+    }
+  };
+
+  return { handlePointerDown, handleClick };
+};
+
+const InstancedPoints = ({ data, layout, selectedPoint, onSelectPoint }) => {
   const meshRef = React.useRef();
   const numPoints = data.length;
-   // run the layout, animating on change
-   useAnimatedLayout({
+
+  // run the layout, animating on change
+  useAnimatedLayout({
     data,
     layout,
     onFrame: () => {
       updateInstancedMeshMatrices({ mesh: meshRef.current, data });
     },
-   });
+  });
+
   // update instance matrices only when needed
   React.useEffect(() => {
     updateInstancedMeshMatrices({ mesh: meshRef.current, data });
   }, [data, layout]);
+
+  const { handleClick, handlePointerDown } = useMousePointInteraction({
+    data,
+    selectedPoint,
+    onSelectPoint,
+  });
+  const { colorAttrib, colorArray } = usePointColors({ data, selectedPoint });
 
   return (
     <instancedMesh
       ref={meshRef}
       args={[null, null, numPoints]}
       frustumCulled={false}
+      onClick={handleClick}
+      onPointerDown={handlePointerDown}
     >
-      <sphereBufferGeometry attach="geometry" args={[1, 35, 35]} />
-      <meshStandardMaterial attach="material" color="#D1D2F9" />
+      <cylinderBufferGeometry attach="geometry" args={[0.5, 0.5, 0.15, 32]}>
+        <instancedBufferAttribute
+          ref={colorAttrib}
+          attachObject={['attributes', 'color']}
+          args={[colorArray, 3]}
+        />
+      </cylinderBufferGeometry>
+      <meshStandardMaterial
+        attach="material"
+        vertexColors={THREE.VertexColors}
+      />
     </instancedMesh>
   );
 };
